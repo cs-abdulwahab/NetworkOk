6a7c829d04bcc74dc4ee4abd2b768a56841a321a
Abdul Wahab <cs.abdulwahab@gmail.com>
2017-10-19 12:00:00 +0500

    Food Item Added

--- app/src/main/java/com/example/abdul_wahab/networkok/MainActivity.java
+++ app/src/main/java/com/example/abdul_wahab/networkok/MainActivity.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.example.abdul_wahab.networkok.models.Food;
+import com.google.gson.Gson;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -20,6 +25,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final OkHttpClient client = new OkHttpClient();
+
+        final Request request = new Request.Builder()
+                .url("http://uol.mocklab.io/food")
+                .get()
+                .addHeader("cache-control", "no-cache")
+                .build();
+
+        // Response response = client.newCall(request).execute();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                Log.d(TAG, "onFailure() called with: call = [" + call + "], e = [" + e + "]");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG, "onResponse: ");
+
+                Gson gson = new Gson();
+                Food []food = gson.fromJson(response.body().string(), Food[].class);
+
+                ArrayList<Food > foodList  = new ArrayList<Food>(Arrays.asList(food)) ;
+
+                String s = "";
+
+            }
+        });
+
+        Log.d(TAG, "end of oncreate method: ");
+
 
     }
 }
