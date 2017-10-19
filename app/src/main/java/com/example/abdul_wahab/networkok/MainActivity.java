e14c77bed07e3d3fb4c17e3e555c57897fabf4ee
Abdul Wahab <cs.abdulwahab@gmail.com>
2017-10-19 11:14:51 +0500

    Initial commit

---
+++ app/src/main/java/com/example/abdul_wahab/networkok/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.abdul_wahab.networkok;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MTAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
